54e03c79e4f3378b90ccac931b2331e6b228ddc3
chitsanuponjate <chitsanupong.j@cloudnatician.com>
2023-06-23 10:57:06 +0700

    update solution2

--- BasicDocker/step2/verify.sh
+++ BasicDocker/step2/verify.sh
@@ -4,8 +4,6 @@ set -e # exit once any command fails
 {
     date
 
-    docker image ls | grep my-ping | grep -v local-registry | grep 1.0.1
-
     docker image ls | grep local-registry:5000/my-ping | grep 1.0.1
 
     curl http://local-registry:5000/v2/my-ping/tags/list -k | grep my-ping | grep 1.0.1
